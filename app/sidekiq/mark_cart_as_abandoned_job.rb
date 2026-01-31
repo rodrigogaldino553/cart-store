360e5e7f18ce899087351360cbb3451c3b9d19b2
Rodrigo <galdinorodrigo553@gmail.com>
2026-01-31 16:33:28 -0300

    fix: mark_as_abandoned_job specs

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,5 +1,6 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
+
   queue_as :default
 
   def perform
@@ -11,7 +12,7 @@ class MarkCartAsAbandonedJob
 
   def mark_abandoned_carts
     carts_to_abandon = Cart.ready_to_abandon
-    carts_to_abandon.find_each(&:mark_as_abandoned!)
+    carts_to_abandon.find_each(&:mark_as_abandoned)
     Rails.logger.info "[MARK_AS_ABANDONED] Marked carts as abandoned"
   end
 
@@ -19,7 +20,7 @@ class MarkCartAsAbandonedJob
     carts_to_delete = Cart.ready_to_delete
     count = carts_to_delete.count
     carts_to_delete.destroy_all
-    
+
     Rails.logger.info "[DELETE_OLD_ABANDONED_CARTS] Deleted #{count} abandoned carts"
   end
 end
